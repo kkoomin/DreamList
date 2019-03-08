fc5d416f9cc27c9a79d1d1c4e98664395c46699d
kkoomin <minha.koo.9@gmail.com>
2019-03-08 10:02:32 +0000

    colored navbar

--- dreamlist-frontend/src/index.js
+++ dreamlist-frontend/src/index.js
@@ -3,10 +3,12 @@ const navUser = document.querySelector("#nav-user-id")
 function setUserGreet() {
     const sessionUserName = sessionStorage.user_name
     if(sessionUserName !== undefined) {
-        navUser.innerText = `Welcome! ${sessionUserName}`
+        navUser.innerText = `Welcome! ${sessionUserName} ☀️`
     } else {
         navUser.innerText = "Welcome!"
     }
 }
 
 setUserGreet()
+
+
