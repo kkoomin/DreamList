0d7c8d44144e512db5e8e6de6ec57980817912f2
kkoomin <minha.koo.9@gmail.com>
2019-04-30 22:39:28 +0100

    add demo gifs

--- README.md
+++ README.md
@@ -4,60 +4,74 @@ iPad application for music students to read music sheets, manage daily schedules
 
 Finding a place for your next vacation could be hard. Most likely it depends on one of the following things: word of mouth, lonely planet top 50 of the year, tour agency packages, or simply where a budget flight ticket takes you. While conversations with your friends on where to go next could be fun, it is also quite limiting. It stops you from finding some of the hidden gems that may have perfectly fit your criteria but just has not been well travelled to yet. (Who would have know that Azerbaijan is so cool?) Destination DreamList will revolutionize this process. We help you triangulate your full potential dream travel destinations.
 
-
 What does Destination DreamList offer?
-  * Power search engine that allows you to input criteria such as weather, price, 'good for x' key words and find the full list of destinations that fits the criteria
-  * Add dream destination to your list, and keep track
-  * Tracking of flight price to a dozen of your dream list cities in just one click
+
+- Power search engine that allows you to input criteria such as weather, price, 'good for x' key words and find the full list of destinations that fits the criteria
+- Add dream destination to your list, and keep track
+- Tracking of flight price to a dozen of your dream list cities in just one click
 
 ## Demo
 
+Login page, main search map page
 
+![Login & Search](dreamlist-search.gif)
 
-## Features 
-/// MVP 
-/// GTF /// Good to have 
-/// ATF /// Amazing to have 
+Search by city name and check result
+
+![Search & Result](dreamlist-search-result.gif)
+
+Get flights information by Skyscanner
+
+![Flight](dreamlist-flight.gif)
+
+Add more holiday period in user page
+
+![User](dreamlist-add-holiday.gif)
+
+## Features
+
+/// MVP
+/// GTF /// Good to have
+/// ATF /// Amazing to have
 
 - MVP features
 
-  * Users
+  - Users
     - Basic profile
     - Home base
     - Vacation periods
-  * Destination search
+  - Destination search
     - Search by economics
       - flight price (average flight price from origin to destination) [SKYSCANNER API]
     - Show result on Map
       - display results on map [GOOGLE MAP API]
-  * Flight price tracking
+  - Flight price tracking
     - Cheapest top 5-10 destinations for each vacation periods
 
 - planned features
-  * Users
+  - Users
     - Basic profile
     - Home base
     - Vacation periods
-  * Destination search
+  - Destination search
     - Search by economics
       - flight price (average flight price from origin to destination) [SKYSCANNER API]
       - hotel price (average hotel price) [TBD SOURCE] /// GTF ///
     - Search by weather
-      - weather of desirable time periods [TBD API]  /// GTF ///
-    - Search by popularity [TBD SOURCE or IN-HOUSE]  /// ATF ///
-    - Search by keywords** [TBD SOURCE or IN-HOUSE]  /// ATF ///
+      - weather of desirable time periods [TBD API] /// GTF ///
+    - Search by popularity [TBD SOURCE or IN-HOUSE] /// ATF ///
+    - Search by keywords\*\* [TBD SOURCE or IN-HOUSE] /// ATF ///
     - Show result on Map
       - display results on map [GOOGLE MAP API]
-      - thumbnails to show overview description with a few pictures [GOOGLE PLACE API]  /// GTF ///
-  * Flight price tracking
+      - thumbnails to show overview description with a few pictures [GOOGLE PLACE API] /// GTF ///
+  - Flight price tracking
     - Cheapest top 5-10 destinations for each vacation periods
     - Link to booking /// ATF ///
 
 ## Technologies
 
-- JavaScript 
+- JavaScript
 - Ruby on Rails
 - Bootstrap
 
 ## Credits
-
--- dreamlist-frontend/src/user.js
+++ dreamlist-frontend/src/user.js
@@ -1,118 +1,127 @@
 function getUserData() {
-    return fetch(`http://localhost:3000/users`)
+  return fetch(`http://localhost:3000/users`)
     .then(res => res.json())
-    .then(data => data.filter(user => user.id === parseInt(sessionStorage.user_id) ))
+    .then(data =>
+      data.filter(user => user.id === parseInt(sessionStorage.user_id))
+    );
 } //user.vacations {id, start_date, end_date, name}
 
 function getHomebase(id) {
-    return fetch(`http://localhost:3000/destinations/${id}/getHomebase`)
+  return fetch(`http://localhost:3000/destinations/${id}/getHomebase`)
     .then(res => res.json())
-    .then(destination => destination.name)
+    .then(destination => destination.name);
 }
 
 function addVacationData(data) {
-    const options = {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(data)
-    }
-    return fetch(`http://localhost:3000/vacations`, options)
-    .then(res => res.json())
+  const options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  };
+  return fetch(`http://localhost:3000/vacations`, options).then(res =>
+    res.json()
+  );
 }
 
 function deleteVacationData(id) {
-    return fetch(`http://localhost:3000/vacations/${id}`, {method: "DELETE"})
+  return fetch(`http://localhost:3000/vacations/${id}`, { method: "DELETE" });
 }
-
 
 //render
 async function renderUserInfo(user) {
-    const userName = document.querySelector(".info-user-name")
-    userName.innerHTML = `<h1>${user[0].name} 👻</h1> from ${await getHomebase(user[0].home_base_id)}`
-    // const userHomebase = document.querySelector(".info-user-info")
-    // userHomebase.innerText = `from ${await getHomebase(user[0].home_base_id)}`
+  const userName = document.querySelector(".info-user-name");
+  userName.innerHTML = `<h1>${user[0].name} 👻</h1> from ${await getHomebase(
+    user[0].home_base_id
+  )}`;
+  // const userHomebase = document.querySelector(".info-user-info")
+  // userHomebase.innerText = `from ${await getHomebase(user[0].home_base_id)}`
 }
 
-const divVacation = document.querySelector(".info-user-vacation")
+const divVacation = document.querySelector(".info-user-vacation");
 
 function renderVacations(vacations) {
-    divVacation.innerHTML = ""
-    divVacation.innerHTML = "<h2 id='vascation-title'>Your Vacations</h2>"
-    const table = document.createElement("table")
-    table.className = "table table-vacation"
-    const tHead = document.createElement("thead")
-    const tr_head = document.createElement("tr")
-    tr_head.innerHTML = `
+  divVacation.innerHTML = "";
+  divVacation.innerHTML = "<h2 id='vascation-title'>Your Vacations</h2>";
+  const table = document.createElement("table");
+  table.className = "table table-vacation";
+  const tHead = document.createElement("thead");
+  const tr_head = document.createElement("tr");
+  tr_head.innerHTML = `
         <th scope="col"></th>
         <th scope="col">Name</th>
         <th scope="col">Date</th>
         <th scope="col">Delete</th>
-    `
-    tHead.appendChild(tr_head)
-    table.appendChild(tHead)
+    `;
+  tHead.appendChild(tr_head);
+  table.appendChild(tHead);
 
-    const tBody = document.createElement("tbody")
-    let counter = 1
-    vacations.forEach(vacation => {
-        const tr_body = document.createElement("tr")
-        tr_body.innerHTML = `
+  const tBody = document.createElement("tbody");
+  let counter = 1;
+  vacations.forEach(vacation => {
+    const tr_body = document.createElement("tr");
+    tr_body.innerHTML = `
             <th scope="row">${counter++}</th>
             <td>${vacation.name}</td>
             <td>${vacation.start_date} ~ ${vacation.end_date}</td>
-            `
-        const td = document.createElement("td")
-        const btn = document.createElement("button")
-        btn.className = "delete-btn"
-        btn.dataset.id = vacation.id
-        btn.innerHTML = `<button type="button" class="btn btn-light"><i class="fas fa-trash-alt"></i></button>`
-        btn.addEventListener("click", e => {
-            deleteVacationData(vacation.id)
-            .then(() => getUserData()
-                .then(user => renderVacations(user[0].vacations)))
-            })
-        td.appendChild(btn)
-        tr_body.appendChild(td)
-        tBody.appendChild(tr_body)
-    })
-    table.appendChild(tBody)
+            `;
+    const td = document.createElement("td");
+    const btn = document.createElement("button");
+    btn.className = "delete-btn";
+    btn.dataset.id = vacation.id;
+    btn.innerHTML = `<button type="button" class="btn btn-light"><i class="fas fa-trash-alt"></i></button>`;
+    btn.addEventListener("click", e => {
+      deleteVacationData(vacation.id).then(() =>
+        getUserData().then(user => renderVacations(user[0].vacations))
+      );
+    });
+    td.appendChild(btn);
+    tr_body.appendChild(td);
+    tBody.appendChild(tr_body);
+  });
+  table.appendChild(tBody);
 
-    const addBtn = document.createElement("button")
-    addBtn.innerText = "Add Vacation"
-    addBtn.className = "btn btn-info"
-    addBtn.addEventListener("click", e => {
-        renderAddVacationForm(e)
-    })
-    divVacation.append(table, addBtn)
+  const addBtn = document.createElement("button");
+  addBtn.innerText = "Add Vacation";
+  addBtn.className = "btn btn-info";
+  addBtn.addEventListener("click", e => {
+    renderAddVacationForm(e);
+  });
+  divVacation.append(table, addBtn);
 }
 
 function renderAddVacationForm(e) {
-    const addFormDiv = document.querySelector(".add-vacation")
-    addFormDiv.style.display === 'none' ? addFormDiv.style.display = 'block' : addFormDiv.style.display = 'none'
+  const addFormDiv = document.querySelector(".add-vacation");
+  addFormDiv.style.display === "none"
+    ? (addFormDiv.style.display = "block")
+    : (addFormDiv.style.display = "none");
 }
 
 function addNewVacation(event) {
-    event.preventDefault()
-    console.log(event.target.elements)
-    const vacationName = event.target.elements["holiday-name"].value
-    const vacationStart = event.target.elements["trip-start"].value
-    const vacationEnd = event.target.elements["trip-end"].value
-    const data = {name: vacationName, start_date: vacationStart, end_date: vacationEnd, user_id: sessionStorage.user_id}
-    event.target.reset()
+  event.preventDefault();
+  console.log(event.target.elements);
+  const vacationName = event.target.elements["holiday-name"].value;
+  const vacationStart = event.target.elements["trip-start"].value;
+  const vacationEnd = event.target.elements["trip-end"].value;
+  const data = {
+    name: vacationName,
+    start_date: vacationStart,
+    end_date: vacationEnd,
+    user_id: sessionStorage.user_id
+  };
+  event.target.reset();
 
-    addVacationData(data)
-    .then(() => getUserData()
-        .then(user => renderVacations(user[0].vacations)))
+  addVacationData(data).then(() =>
+    getUserData().then(user => renderVacations(user[0].vacations))
+  );
 }
-
 
 function init() {
-    const addForm = document.querySelector("#add-vacation-form")
-    addForm.addEventListener("submit", event => addNewVacation(event))
-    getUserData()
-    .then(user => {
-        renderUserInfo(user)
-        renderVacations(user[0].vacations)
-    })
+  const addForm = document.querySelector("#add-vacation-form");
+  addForm.addEventListener("submit", event => addNewVacation(event));
+  getUserData().then(user => {
+    renderUserInfo(user);
+    renderVacations(user[0].vacations);
+  });
 }
 
-init()
+init();
